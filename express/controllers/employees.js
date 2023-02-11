f39e1cac064b9e092e865d460d0804f1f91dbef8
hidoominik <dominik.choma.41@gmail.com>
2023-02-11 12:46:01 +0100

    find in range and module exports refactor

--- express/controllers/employees.js
+++ express/controllers/employees.js
@@ -2,7 +2,8 @@
 // import db from '../models/index.js';
 const db = require('../models');
 
-module.exports = getEmployees = async(req, res) => {
+module.exports = {
+    getEmployees: getEmployees = async(req, res) => {
     try {
         let employees = await db.Employee.findAll();
         // res.json({message: 'Success'});
@@ -11,4 +12,28 @@ module.exports = getEmployees = async(req, res) => {
         res.status(404).json({message: error.message});
         console.log(error.message)
     }
+},
+
+    getEmployeesInRange: getEmployeesInRange = async(req, res) =>{
+    const {idFrom: idFrom, idTo: idTo} = req.params;
+    try {
+        if(idFrom < idTo){
+            const employeeFrom = await db.Employee.findByPk(idFrom);
+            const employeeTo = await db.Employee.findByPk(idTo);
+            if(!employeeFrom || !employeeTo) res.status(404).json({message: 'Incorrect range!'});
+            console.log(idFrom);
+            console.log(idTo);
+            const range = (start, stop) => Array.from({ length: stop - start + 1 }, (_, i) => start + i);
+            console.log(range(idFrom, idTo));
+            const employees = await db.Employee.findAll({
+                where:{emp_no: range(parseInt(idFrom), parseInt(idTo))}
+            })
+            res.status(200).json(employees);
+        }
+        res.status(400).json({message: 'Incorrect range!'});
+    } catch (error) {
+        
+    }
+
+}
 }
--- express/routes/employee.js
+++ express/routes/employee.js
@@ -2,10 +2,9 @@
 // import {getEmployees } from '../controllers/employees.js'
 
 const express = require('express');
-const getEmployees = require('../controllers/employees.js')
-
+const employees = require('../controllers/employees.js')
 const router = express.Router();
 
-router.get('/', getEmployees);
-
+router.get('/fetchAll', employees.getEmployees);
+router.get('/fetchInRange/:idFrom/:idTo', employees.getEmployeesInRange);
 module.exports = router;
