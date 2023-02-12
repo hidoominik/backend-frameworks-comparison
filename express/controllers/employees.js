bde971a7d35b8eee97b3119f9ab72781f1dd2403
hidoominik <dominik.choma.41@gmail.com>
2023-02-12 21:03:20 +0100

    delete unnecessary code

--- express/controllers/employees.js
+++ express/controllers/employees.js
@@ -4,44 +4,16 @@ const db = require('../models');
 
 module.exports = {
     getEmployees: getEmployees = async(req, res) => {
-    try {
-        let employees = await db.Employee.findAll();
-        // res.json({message: 'Success'});
-        res.status(200).json(employees);
-    } catch (error) {
-        res.status(404).json({message: error.message});
-        console.log(error.message)
-    }
-},
-
-    getEmployeesInRange: getEmployeesInRange = async(req, res) =>{
-    const {idFrom: idFrom, idTo: idTo} = req.params;
-    try {
-        if(idFrom < idTo){
-            const employeeFrom = await db.Employee.findByPk(idFrom);
-            const employeeTo = await db.Employee.findByPk(idTo);
-            if(!employeeFrom || !employeeTo){
-               res.status(404).json({message: 'Incorrect range!'}); 
-            } else {
-                const range = (start, stop) => Array.from({ length: stop - start + 1 }, (_, i) => start + i);
-                console.log(range(idFrom, idTo));
-                const employees = await db.Employee.findAll({
-                    where:{emp_no: range(parseInt(idFrom), parseInt(idTo))}
-                })
-                res.status(200).json(employees);
-            }
-           
-            
-        }else{
-            res.status(400).json({message: 'Incorrect range!'});
+        try {
+            let employees = await db.Employee.findAll();
+            // res.json({message: 'Success'});
+            res.status(200).json(employees);
+        } catch (error) {
+            res.status(404).json({message: error.message});
+            console.log(error.message)
         }
-        
-    } catch (error) {
-        res.status(404).json({message: error.message});
-        console.log(error.message)
-    }
+    },
 
-},
     getOne: getOne = async(req,res)=>{
         const {id: id} = req.params;
         try {
--- express/routes/employee.js
+++ express/routes/employee.js
@@ -6,7 +6,6 @@ const employees = require('../controllers/employees.js')
 const router = express.Router();
 
 router.get('/fetchAll', employees.getEmployees);
-router.get('/fetchInRange/:idFrom/:idTo', employees.getEmployeesInRange);
 router.get('/:id', employees.getOne);
 router.post('/create', employees.createOne);
 router.delete('/delete/:id', employees.deleteOne);
